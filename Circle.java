3a84f9ce9a2cfd38072983e5b5f66357ab7df2bd
Sipokuhle Gulubele <100310603+Kuhlegulubela@users.noreply.github.com>
2022-04-21 13:10:52 +0200

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,32 @@
+package za.wsu.ac.s2190328087;
+
+public class Circle {
+	private double radius;
+	private int x;
+	private int y;
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+}
