332bbf8c4bebd3a95b9150c2abc3dfb07a48f1cb
Sipokuhle Gulubele <100310603+Kuhlegulubela@users.noreply.github.com>
2022-04-21 13:29:10 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -30,3 +30,33 @@ public class Circle {
 	}
 	
 }
+
+import java.util.Scanner;
+
+public class CircleApp{
+	public static void main(String[] args) {
+		float radius = 0; int x; int y;
+		double area, circumference = 0, diameter=0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter radius of the circle:");
+		radius = input.nextFloat();
+		
+		System.out.println("Enter the x coordinate of the circle:");
+		x = input.nextInt();
+		
+		System.out.println("Enter the y coordinate of the circle:");
+		y =input.nextInt();
+		
+		area = Math.PI * radius * radius;
+		System.out.println("Area of the circle:" + area);
+		
+		circumference = 2 * Math.PI * radius;
+		System.out.println("Circumference of the circle:" + circumference);
+		
+		diameter = 2 * radius;
+		System.out.println("Diameter of the circle:" + diameter);
+		
+	}
+	
+}
